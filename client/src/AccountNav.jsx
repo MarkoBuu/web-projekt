f710c9f9c61a2935f887855bd8e3e5f8e3cf56b8
MarkoBuu <marko.budak33@gmail.com>
2024-06-15 20:00:28 +0200

    Initial commit

---
+++ client/src/AccountNav.jsx
@@ -0,0 +1,45 @@
+import {Link, useLocation} from "react-router-dom";
+
+export default function AccountNav() {
+    const {pathname} = useLocation();
+    let subpage = pathname.split("/")?.[2];
+    if(subpage === undefined){
+        subpage = "profile";
+    }
+    function linkClasses(type=null){
+
+        let classes = "inline-flex gap-1 p-2 px-6 rounded-full";
+        if(type === subpage){
+            classes += " bg-primary text-white ";
+        }else{
+            classes += " bg-gray-100";
+        }
+        return classes;
+    }
+
+    return (
+        <nav className="w-full flex justify-center mt-8 gap-2 mb-8">
+            <Link className={linkClasses("profile")} to={"/account"}>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5}
+                     stroke="currentColor" className="size-6">
+                    <path strokeLinecap="round" strokeLinejoin="round"
+                          d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"/>
+                </svg>
+                My profile </Link>
+            <Link className={linkClasses("bookings")} to={"/account/bookings"}>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5}
+                     stroke="currentColor" className="size-6">
+                    <path strokeLinecap="round" strokeLinejoin="round"
+                          d="M12 6.042A8.967 8.967 0 0 0 6 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 0 1 6 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 0 1 6-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0 0 18 18a8.967 8.967 0 0 0-6 2.292m0-14.25v14.25"/>
+                </svg>
+                My bookings </Link>
+            <Link className={linkClasses("places")} to={"/account/places"}>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5}
+                     stroke="currentColor" className="size-6">
+                    <path strokeLinecap="round" strokeLinejoin="round"
+                          d="M8.25 21v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21m0 0h4.5V3.545M12.75 21h7.5V10.75M2.25 21h1.5m18 0h-18M2.25 9l4.5-1.636M18.75 3l-1.5.545m0 6.205 3 1m1.5.5-1.5-.5M6.75 7.364V3h-3v18m3-13.636 10.5-3.819"/>
+                </svg>
+                My accommodations </Link>
+        </nav>
+    );
+}
--- client/src/pages/IndexPage.jsx
+++ client/src/pages/IndexPage.jsx
@@ -1,6 +1,29 @@
+import {useEffect, useState} from "react";
+import axios from "axios";
+
 export default function IndexPage(){
+    const [places, setPlaces] = useState([]);
+    useEffect(() => {
+        axios.get("/places").then((response) => {
+            setPlaces(response.data);
+        });
+    }, []);
     return (
-        <div>
-           index page here
-        </div>)
+        <div className="mt-8 grid gap-8 grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+            {places.length > 0 && places.map(place => (
+                <div>
+                    <div className= "bg-gray-500 mb-3 rounded-2xl flex">
+                        {place.photos?.[0] && (
+                            <img className="rounded-xl object-cover aspect-square" src ={"http://localhost:4000/uploads"+place.photos?.[0]} alt="" />
+                        )}
+                    </div>
+                    <h3 className="font-bold text-center ">{place.title}</h3>
+                    <h2 className="text-sm text-gray-600 text-center "> {place.address}</h2>
+                    <div className="text-center">
+                        <span className="font-bold">{place.maxGuests}€</span>
+                    </div>
+                </div>
+            ))}
+        </div>
+    );
 }
